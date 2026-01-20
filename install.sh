28947bf0484e4bd4133eb824bf65b1f9a0885fe6
John <github.unruly743@silomails.com>
2026-01-19 18:21:59 -0800

    Create install script for workspace setup
    This script sets up a workspace by symlinking dotfiles, installing various tools, and configuring settings.

---
+++ install.sh
@@ -0,0 +1,108 @@
+#!/usr/bin/env bash
+
+# If you remove the .example extension from the file, once your workspace is created and the contents of this
+# repo are copied into it, this script will execute.  This will happen in place of the default behavior of the workspace system,
+# which is to symlink the dotfiles copied from this repo to the home directory in the workspace.
+#
+# Why would one use this file in stead of relying upon the default behavior?
+#
+# Using this file gives you a bit more control over what happens.
+# If you want to do something complex in your workspace setup, you can do that here.
+# Also, you can use this file to automatically install a certain tool in your workspace, such as vim.
+#
+# Just in case you still want the default behavior of symlinking the dotfiles to the root,
+# we've included a block of code below for your convenience that does just that.
+
+set -euo pipefail
+set -x
+
+# Print the line number and command that caused the failure
+trap 'echo "Error on line $LINENO: $BASH_COMMAND"' ERR
+
+DOTFILES_PATH="$HOME/dotfiles"
+
+# Symlink dotfiles to the root within your workspace
+find $DOTFILES_PATH -type f -path "$DOTFILES_PATH/.*" |
+while read df; do
+  link=${df/$DOTFILES_PATH/$HOME}
+  mkdir -p "$(dirname "$link")"
+  ln -sf "$df" "$link"
+done
+
+# https://github.com/zsh-users/zsh-autosuggestions/blob/master/INSTALL.md#oh-my-zsh
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# https://github.com/zsh-users/zsh-syntax-highlighting/blob/master/INSTALL.md#oh-my-zsh
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# https://github.com/eza-community/eza/blob/main/INSTALL.md#clone-the-repository
+git clone https://github.com/eza-community/eza.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/completions/eza
+
+# wait because github may return 429 (too many requests)
+sleep .5
+
+# https://medium.com/@almatins/install-nerdfont-or-any-fonts-using-the-command-line-in-debian-or-other-linux-f3067918a88c
+wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/FiraCode.zip \
+    && cd ~/.local/share/fonts \
+    && unzip FiraCode.zip \
+    && rm FiraCode.zip \
+    && fc-cache -fv
+
+# https://github.com/eza-community/eza/blob/main/INSTALL.md#debian-and-ubuntu
+sudo apt-get update
+sudo apt-get --assume-yes install gpg
+
+# wait because github may return 429 (too many requests)
+sleep .5
+
+# # https://github.com/eza-community/eza/blob/main/INSTALL.md#debian-and-ubuntu
+sudo mkdir -p /etc/apt/keyrings
+wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --batch --dearmor -o /etc/apt/keyrings/gierens.gpg
+echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+sudo apt-get update
+sudo apt-get --assume-yes install eza
+
+# https://starship.rs/guide/
+curl -sS https://starship.rs/install.sh | sh -s -- -y
+
+# wait because github may return 429 (too many requests)
+sleep .5
+
+# https://github.com/cli/cli/blob/trunk/docs/install_linux.md#debian-ubuntu-linux-raspberry-pi-os-apt
+(type -p wget >/dev/null || (sudo apt-get update && sudo apt-get install wget -y)) \
+	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+        && out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+        && cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+	&& sudo apt-get update \
+	&& sudo apt-get install gh -y
+
+# install rapid
+update-tool rapid
+
+# install git dd
+# https://datadoghq.atlassian.net/wiki/spaces/FF/pages/5695472050/Git+dd#On-macOS
+update-tool git-dd
+
+# wait because github may return 429 (too many requests)
+sleep .5
+
+# https://github.com/ajeetdsouza/zoxide?tab=readme-ov-file#installation
+curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
+
+# Setup FS Monitoring
+# https://datadoghq.atlassian.net/wiki/spaces/FF/pages/4594926370/Git+performance+settings+for+large+repos#core.fsmonitor
+## Install watchman
+sudo apt-get --assume-yes install watchman
+## Copy the sample hook from the git repo to your hooks dir (need to be done per-repo)
+curl https://raw.githubusercontent.com/git/git/refs/tags/v2.49.0/templates/hooks/fsmonitor-watchman.sample > .git/hooks/fsmonitor-watchman && chmod +x .git/hooks/fsmonitor-watchman
+## Enable fsmonitor (need to be done per-repo)
+git config --local core.fsmonitor .git/hooks/fsmonitor-watchman
+
+# Setup git branch prefixe
+git dd add-branch-prefix john.emau
+
+# https://github.com/pup-mikegraf/workspace-dotfiles/blob/87fcbaff976e2c20a09ff3d0d8fd2e42553d09a2/install.sh#L32C1-L32C32
+touch $HOME/.dotfiles_installed
